77694e0035d788fb66587a03ad6b9695d121f725
kennedyshearer <kshea009@gmail.com>
2025-07-16 22:50:53 -0400

    Add links, specify language, limit news, add subject, control category and up-to-date news

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
+import time
 from send_email import send_email
 
+date = time.strftime("%Y-%m-%d")
+topic = "tesla"
+
 api_key = "a0860cae40a9404ca4315c8bf416c069"
-url = ("https://newsapi.org/v2/everything?q=tesla&"
-       "from=2025-06-15&sortBy=publishedAt&apiKey="
-       "a0860cae40a9404ca4315c8bf416c069")
+url = "https://newsapi.org/v2/everything?" \
+       f"q={topic}&" \
+       f"from={date}&sortBy=publishedAt&apiKey=" \
+       "a0860cae40a9404ca4315c8bf416c069&" \
+       "language=en"
 
 # Make a request
 r = requests.get(url)
@@ -14,11 +20,15 @@ content = r.json()
 
 # Access the articles titles adn description
 body = ""
-for article in content['articles']:
+for article in content['articles'][:20]:
     if article['title'] is not None or article['description'] is not None:
         title = article['title'] or ""
         description = article['description'] or ""
-        body = body + title + "\n" + description + 2*"\n"
+        url_link = article['url'] or ""
+        body = "Subject: Today's news" \
+               + "\n" + body + title \
+               + "\n" + description \
+               + "\n" + url_link + 2*"\n"
 
 
 body = body.encode("utf-8")
